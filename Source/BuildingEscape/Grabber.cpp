f699276d7c5442b44baeda6c70e968a5ab059716
Arc-E-Tect <ieising@hotmail.com>
2022-02-07 20:59:36 +0100

    Lesson 120

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -43,13 +43,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		OUT PlayerViewPointRotation
 	);
 
-	// UE_LOG(LogTemp, Warning, TEXT("Location:%s, Rotation:%s"), 
-	// 	*PlayerViewPointLocation.ToString(), 
-	// 	*PlayerViewPointRoation.ToString()
-	// );
-
-	// Draw a line from player showing the reach.
-
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 
 	DrawDebugLine(
@@ -62,8 +55,27 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0,
 		5.f
 	);
-	// Logging out to test
 
+	FHitResult Hit;
+	// Ray-cast out to a certain distance (Reach)
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	// See what it hits
+	AActor* ActorHit = Hit.GetActor();
+
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Line trace has hit: %s"), *(ActorHit->GetName()))
+	}
+	
 	// Ray-cast out to a certain distance (Reach)
 
 	// See what it hits
